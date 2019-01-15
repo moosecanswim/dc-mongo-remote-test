193a68655897ff8855c4aaac497568cc199515f4
moosecanswim <kyle.gallagher@deciphernow.com>
2019-01-15 13:52:55 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dc-mongo-remote-test
 Try out running mongo as a remote service
+
+This project was to test out how to connect an app to a remote service of mongo.  also brought in some viper for setting mannagment.
+
+When set up this way viper will pull in environmental variables overriding the settings.toml file.  NEAT!
+
+Based on this tutorial about maintaining a clean development enviroment with docker: https://outcrawl.com/clean-development-environment-docker , https://github.com/tinrab/go-mongo-docker-example
+
+TODO:
+play with the nginx because the static site did not deploy correctly
---
+++ api/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"github.com/spf13/viper"
+	"gopkg.in/mgo.v2"
+)
+
+type Post struct {
+	Text      string    `json:"text" bson:"text"`
+	CreatedAt time.Time `json:"createdAt" bson:"created_at"`
+}
+
+var posts *mgo.Collection
+
+func main() {
+	// Connect to mongo
+	viper.SetConfigName("settings")
+	viper.AddConfigPath("$GOPATH/src/app/")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("/go/src/app")
+
+	viper.WatchConfig()
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalln(fmt.Errorf("Fatal error config file %s \n", err))
+	}
+
+	username := viper.GetString("mongo_user")
+	password := viper.GetString("mongo_password")
+	database := viper.GetString("mongo_db")
+	// address := viper.GetString("mongo_hosts")
+	address := viper.GetString("mongo_host")
+
+	log.Println("my name is")
+	log.Println(viper.GetString("name"))
+
+	log.Println("Viper config address")
+	log.Println(address)
+
+	var connURL string
+	if username == "" || password == "" {
+		connURL = fmt.Sprintf("mongodb://%s/%s", address, database)
+	} else {
+		connURL = fmt.Sprintf("mongodb://%s:%s@%s/%s", username, password, address, database)
+	}
+	log.Println(connURL)
+	session, err := mgo.Dial(connURL)
+	if err != nil {
+		log.Fatalln(err)
+		log.Fatalln("mongo err")
+		os.Exit(1)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	// Get posts collection
+	posts = session.DB("app").C("posts")
+
+	// Set up routes
+	r := mux.NewRouter()
+	r.HandleFunc("/posts", createPost).
+		Methods("POST")
+	r.HandleFunc("/posts", readPosts).
+		Methods("GET")
+
+	http.ListenAndServe(":8080", cors.AllowAll().Handler(r))
+	log.Println("Listening on port 8080...")
+}
+
+func createPost(w http.ResponseWriter, r *http.Request) {
+	// Read body
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responseError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Read post
+	post := &Post{}
+	err = json.Unmarshal(data, post)
+	if err != nil {
+		responseError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	post.CreatedAt = time.Now().UTC()
+
+	// Insert new post
+	if err := posts.Insert(post); err != nil {
+		responseError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	responseJSON(w, post)
+}
+
+func readPosts(w http.ResponseWriter, r *http.Request) {
+	result := []Post{}
+	if err := posts.Find(nil).Sort("-created_at").All(&result); err != nil {
+		responseError(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		responseJSON(w, result)
+	}
+}
+
+func responseError(w http.ResponseWriter, message string, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(map[string]string{"error": message})
+}
+
+func responseJSON(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
